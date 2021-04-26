94f77cdff238e3bdc979cf30de5ed226a9829757
Sergey <cabyrkih@mail.ru>
2021-04-26 12:47:14 +0300

    tasks

---
+++ gulp-2/gulp/tasks/html.js
@@ -0,0 +1,26 @@
+import gulp from 'gulp';
+import include from 'gulp-file-include';
+import htmlmin from 'gulp-htmlmin';
+import gulpif from 'gulp-if';
+import rename from 'gulp-rename';
+import plumber from 'gulp-plumber';
+import config from '../config';
+
+const html = (callback) => {
+   gulp.src(config.src.html)
+      .pipe(plumber())
+      .pipe(include())
+      .pipe(gulp.dest(config.build.html))
+      .pipe(gulpif(config.isProd, htmlmin({
+         collapseWhitespace: true,
+         removeComments: true,
+      })))
+      .pipe(gulpif(config.isProd, rename({
+         suffix: '.min',
+         extname: '.html',
+      })))
+      .pipe(gulpif(config.isProd, gulp.dest(config.build.html)));
+   callback();
+};
+
+export default html;
--- gulp-2/gulpfile.babel.js
+++ gulp-2/gulpfile.babel.js
@@ -3,12 +3,14 @@ import config from './gulp/config';
 import server from './gulp/tasks/server';
 import clean from './gulp/tasks/clean';
 import { scriptBuild, scriptWatch } from './gulp/tasks/scripts';
+import html from './gulp/tasks/html';
 
 config.setEnv();
 
 export const build = gulp.series(
    clean,
    scriptBuild,
+   html,
 );
 
 export const watch = gulp.series(
