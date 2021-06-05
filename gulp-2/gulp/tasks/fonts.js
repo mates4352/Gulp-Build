0b2ad56f22a490960d27007f5906d683bc7c1000
Sergey <cabyrkih@mail.ru>
2021-06-05 21:42:10 +0300

    gulp _scss

--- gulp-2/gulp/tasks/fonts.js
+++ gulp-2/gulp/tasks/fonts.js
@@ -17,7 +17,7 @@ export const fonts = (cb) => {
 }
 
 const cb = () => {}
-const PATH_FONTS_STYLE = 'src/scss/base/fonts.scss'
+const PATH_FONTS_STYLE = 'src/scss/base/_fonts.scss'
 const PATH__FONTS = 'src/assets/fonts'
 
 export const fontsFile = (params) => {
